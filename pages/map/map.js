351759dabc28a3dfeded1afb786531474704e62a
LEWIS\bzluy <bzluyang@126.com>
2017-08-22 20:25:53 +0800

    map

--- pages/map/map.js
+++ pages/map/map.js
@@ -1,114 +1,34 @@
-// pages/movie/movie.js
+//获取应用实例  
+var app = getApp()
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-    markers: [{
-      iconPath: "/icon/weizhi.png",
-      id: 0,
-      latitude: 23.099994,
-      longitude: 113.324520,
-      width: 50,
-      height: 50
-    }],
-    polyline: [{
-      points: [{
-        longitude: 113.3245211,
-        latitude: 23.10229
-      }, {
-        longitude: 113.324520,
-        latitude: 23.21229
-      }],
-      color: "#FF0000DD",
-      width: 2,
-      dottedLine: true
-    }],
-    controls: [{
-      id: 1,
-      iconPath: '/icon/point.png',
-      position: {
-        left: 0,
-        top: 600 - 50,
-        width: 50,
-        height: 50
-      },
-      clickable: true
+    latitude: 0,//纬度  
+    longitude: 0,//经度  
+    speed: 0,//速度  
+    accuracy: 16,//位置精准度  
+    markers: [],
+    covers: [],
+  },
+  onLoad: function () {
+    this.getlocation();
+  },
+  getlocation: function () {
+    var markers = [{
+      latitude: 31.379376702982793,
+      longitude: 121.4922559261322 ,
+      name: '浦东新区',
+      desc: '我的位置'
     }]
-  },
-  regionchange(e) {
-    console.log(e.type)
-  },
-  markertap(e) {
-    console.log(e.markerId)
-  },
-  controltap(e) {
-    console.log(e.controlId)
-  },
-  regionchange(e) {
-    console.log(e.type)
-  },
-  markertap(e) {
-    console.log(e.markerId)
-  },
-  controltap(e) {
-    console.log(e.controlId)
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+    var covers = [{
+      latitude: 31.379376702982793,
+      longitude: 121.4922559261322,
+      rotate: 0
+    }]
+    this.setData({
+      latitude: 31.379376702982793,
+      longitude: 121.4922559261322,
+      markers: markers,
+      covers: covers,
+    })
   }
-})
+})  
